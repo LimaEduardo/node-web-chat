cb0269de62302092034c1cc8de809547dc06fdf2
limaeduardo <eduardo.fernandodelima@gmail.com>
2018-10-16 21:18:53 -0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server')
-
-  socket.emit("createMessage", {
-    from: 'Eduardo',
-    text: "Hello World!"
-  })
 })
 
 socket.on('disconnect', function() {
