a0f07eec853859c3a3ac37eb02c2b358a98898f4
limaeduardo <eduardolima.dev@gmail.com>
2018-11-21 21:41:56 -0200

    add Users class and updateUserList event

--- server/server.js
+++ server/server.js
@@ -4,20 +4,37 @@ const express = require("express")
 const socketIO = require("socket.io")
 
 const {generateMessage, generateLocationMessage} = require("./utils/message")
+const {isRealString} = require('./utils/validators')
+var {Users} = require('./utils/users')
+
 const publicPath = path.join(__dirname, '../public')
 const PORT = process.env.PORT || 3000
 var app = express();
 var server = http.createServer(app)
 var io = socketIO(server)
+var users = new Users()
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected!')
 
-  socket.emit('newMessage', generateMessage('admin','Welcome to the chat app'))
+  socket.on('join', (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)){
+      return callback('Name and room name are required')
+    }
 
-  socket.broadcast.emit('newMessage', generateMessage('admin', 'A new user joined'))
+    socket.join(params.room);
+    users.removeUser(socket.id);
+    users.addUser(socket.id, params.name, params.room);
+
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room))
+
+    socket.emit('newMessage', generateMessage('admin','Welcome to the chat app'))
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('admin', `${params.name} has joined this room`))
+
+    callback()
+  })
 
   socket.on("createMessage", (message, callback) => {
     console.log("CreateMessage", message)
@@ -30,7 +47,12 @@ io.on('connection', (socket) => {
   })
 
   socket.on('disconnect', () => {
-    console.log("User has disconnected")
+    var user = users.removeUser(socket.id)
+
+    if (user) {
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room))
+      io.to(user.room).emit(`newMessage`, generateMessage('Admin', `${user.name} has left the room`))
+    }
   })
 })
 
